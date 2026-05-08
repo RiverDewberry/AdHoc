642aa68eacd4bfb4ce96f333cea174bdfb5661bc
River Dewberry <riverdewberry@gmail.com>
2026-05-07 22:57:52 -0400

    add gameloop and update systems

--- src/root.zig
+++ src/root.zig
@@ -1,28 +1,137 @@
 const std = @import("std");
+const rl = @import("raylib");
 const Io = std.Io;
 
 pub const Entity = i32;
 
 pub const System = struct {
-    ///system struct type
-    T: type,
+    ///components for entities in the system
+    C: type,
+
+    ///system functions
+    F: type,
+
+    ///system global data, should have init and deinit functions
+    D: type,
 
     name: []const u8,
 
     pub fn init(
-        comptime T: type,
+        comptime Component: type,
+        comptime Data: type,
+        comptime Functions: type,
         comptime name: []const u8
         ) System
     {
 
+        inline for (std.meta.fields(Functions)) |fns| {
+            if (!std.meta.hasFn(Functions, fns.name)) {
+                @compileError(name ++ " Function." ++ fns.name ++ " is not a function");
+            }
+        }
+
+        inline for (std.meta.declarations(Data)) |dat| {
+            if (
+                !((comptime std.mem.eql(u8, "init", dat.name)) or
+                (comptime std.mem.eql(u8, "deinit", dat.name)))
+            )
+            {
+                if (std.meta.hasFn(Data, dat.name) ) {
+                    @compileError(name ++ " Data." ++ dat.name ++ " is a function");
+                }
+            }
+        }
+
+        inline for (std.meta.declarations(Component)) |com| {
+            if (
+                !((comptime std.mem.eql(u8, "init", com.name)) or
+                (comptime std.mem.eql(u8, "deinit", com.name)))
+            )
+            {
+                if (std.meta.hasFn(Component, com.name)) {
+                    @compileError(name ++ " Component." ++ com.name ++ " is a function");
+                }
+            }
+        }
+
+        if (!std.meta.hasFn(Data, "init"))
+        {
+            @compileError(name ++ " Data does not have init function");
+        }
+
+        if (!std.meta.hasFn(Data, "deinit"))
+        {
+            @compileError(name ++ " Data does not have deinit function");
+        }
+        
+        if (!std.meta.hasFn(Component, "init"))
+        {
+            @compileError(name ++ " Component does not have init function");
+        }
+
+        if (!std.meta.hasFn(Component, "deinit"))
+        {
+            @compileError(name ++ " Component does not have deinit function");
+        }
+
         return System {
-            .T = T,
+            .C = Component,
+            .D = Data,
+            .F = Functions,
             .name = name
         };
     }
 };
 
-fn AdHocType(comptime systems: []const System) type {
+pub fn SystemStructure(comptime system: System) type
+{
+    return struct {
+        const Self = @This();
+
+        data: system.D,
+        functions: system.F,
+        components: std.AutoHashMap(Entity, system.C),
+
+        pub fn init(_: Self, allocator: std.mem.Allocator) Self
+        {
+            return Self {
+                .data = system.D.init(),
+                .functions = .{},
+                .components = .init(allocator)
+            };
+        }
+
+        pub fn deinit(self: *Self) void
+        {
+            var componentList = self.components.iterator();
+
+            while (componentList.next()) |entry|
+            {
+                entry.value_ptr.deinit();
+            }
+
+            self.components.deinit();
+            self.data.deinit();
+        }
+
+        pub fn addEntity(self: *Self, entity: Entity) void
+        {
+            self.components.put(entity, system.C.init()) catch @panic("out of mem :(");
+        }
+
+        pub fn hasEntity(self: Self, entity: Entity) bool
+        {
+            return self.components.contains(entity);
+        }
+
+        pub fn removeEntity(self: *Self, entity: Entity) void
+        {
+            _ = self.components.remove(entity);
+        }
+    };
+}
+
+fn AdHocType(comptime systems: []const System, comptime gameLoop: fn (anytype) void) type {
 
     comptime var systemNames: [systems.len][]const u8 = undefined;
     comptime var systemTypes: [systems.len]type = undefined;
@@ -31,7 +140,7 @@ fn AdHocType(comptime systems: []const System) type {
     inline for (systems, 0..) |sys, i|
     {
         systemNames[i] = sys.name;
-        systemTypes[i] = sys.T;
+        systemTypes[i] = SystemStructure(sys);
         attrs[i] = std.builtin.Type.StructField.Attributes{
             .@"align" = null, .@"comptime" = false, .default_value_ptr = null
         };
@@ -64,7 +173,8 @@ fn AdHocType(comptime systems: []const System) type {
             inline for (std.meta.fields(@TypeOf(self.systems))) |sys| {
                 if (@as(sys.type, @field(self.systems, sys.name)).hasEntity(entity))
                 {
-                    @as(sys.type, @field(self.systems, sys.name)).removeEntity(entity);
+                    var system = @as(sys.type, @field(self.systems, sys.name));
+                    system.removeEntity(entity);
                 }
             }
 
@@ -82,16 +192,35 @@ fn AdHocType(comptime systems: []const System) type {
             self.entities.deinit(self.allocator);
 
             inline for (std.meta.fields(@TypeOf(self.systems))) |sys| {
-                @as(sys.type, @field(self.systems, sys.name)).deinit();
+                var system = @as(sys.type, @field(self.systems, sys.name));
+                system.deinit();
+            }
+
+            rl.closeWindow();
+        }
+
+        pub fn runGameLoop(self: Self) void
+        {
+            while (!rl.windowShouldClose())
+            {
+                rl.beginDrawing();
+                gameLoop(self);
+                rl.endDrawing();
             }
         }
     };
 }
 
-pub fn init(allocator: std.mem.Allocator, comptime systems: []const System)
-    AdHocType(systems)
+pub fn init(
+    allocator: std.mem.Allocator,
+    comptime systems: []const System,
+    comptime gameLoop: fn (anytype) void,
+    windowName: [:0]const u8,
+    windowWidth: i32,
+    windowHeight: i32)
+    AdHocType(systems, gameLoop)
 {
-    var retVal: AdHocType(systems) = .{
+    var retVal: AdHocType(systems, gameLoop) = .{
         .entities = std.ArrayList(Entity).empty,
         .allocator = allocator,
         .systems = undefined
@@ -100,8 +229,9 @@ pub fn init(allocator: std.mem.Allocator, comptime systems: []const System)
     inline for (std.meta.fields(@TypeOf(retVal.systems))) |sys| {
         @field(retVal.systems, sys.name) = @as(
             sys.type, @field(retVal.systems, sys.name)
-        ).init();
+        ).init(allocator);
     }
 
+    rl.initWindow(windowWidth, windowHeight, windowName);
     return retVal;
 }
